36663cf49de9f50ebacd9918725303b717ecdd5e
John Capone <jicapone@astro.umd.edu>
2013-11-26 15:13:47 -0500

    correction to startup.sh

--- startup.sh
+++ startup.sh
@@ -6,8 +6,9 @@
 PROJECT_ROOT="$(cd $(dirname "."); pwd)"
 
 # add RATIR IDL directories to IDL_PATH
-export IDL_PATH=$IDL_PATH:$IDL_DIR/lib:$PROJECT_ROOT/code/idl:$PROJECT_ROOT/code/photometry4
+export IDL_PATH=$IDL_PATH:+$IDL_DIR/lib:+$PROJECT_ROOT/code/idl:+$PROJECT_ROOT/code/photometry4
 
 # add RATIR python directories to PYTHONPATH
 export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT/code/photometry4/python
+
 
