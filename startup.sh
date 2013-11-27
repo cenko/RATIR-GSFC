70c3620296d3036979dc3ef4f9abb82e674a95a2
Vicki Toy <vickitoy@gmail.com>
2013-11-27 10:23:06 -0500

    Altered statup script to reflect new directory names (after merge resolution)

--- startup.sh
+++ startup.sh
@@ -5,7 +5,7 @@
 #PROJECT_ROOT="$(cd $(dirname "$0"); pwd)"
 PROJECT_ROOT="$(cd $(dirname "."); pwd)"
 
-IDL_PATH=+$IDL_DIR/lib:+$PROJECT_ROOT/code/idl:+$PROJECT_ROOT/code/photometry4
+IDL_PATH=+$IDL_DIR/lib:+$PROJECT_ROOT/code/reduction:+$PROJECT_ROOT/code/photometry
 export IDL_PATH
 
-export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT/code/photometry4
+export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT/code/photometry
