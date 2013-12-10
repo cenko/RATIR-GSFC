18866669885c19f98d20cbbb3a231d5ff4b5242c
Vicki Toy <vickitoy@gmail.com>
2013-12-10 13:18:06 -0500

    Added autoredux file

---
+++ code/photometry/python/autoredux.py
@@ -0,0 +1,12 @@
+import icoords
+import calcoff
+import assoc
+import finalphot
+import plotratir
+
+def autoredux():
+	icoords.icoords()
+	calcoff.calcoff()
+	assoc.assoc()
+	finalphot.finalphot()
+	plotratir.plotratir()
