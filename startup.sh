a3417568a2067e8eb6497c41c8f8d2ef237a78eb
John Capone <jicapone@astro.umd.edu>
2014-02-07 09:45:49 -0500

    housekeeping

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@
 #PROJECT_ROOT="$(cd $(dirname "."); pwd)"
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )" # allows startup.sh to be called from any directory.  will fail if last component of path to startup.sh is a symlink.
 
-IDL_PATH=+$IDL_DIR/lib:+$PROJECT_ROOT/code/reduction:+$PROJECT_ROOT/code/photometry
+IDL_PATH=$IDL_PATH:+$PROJECT_ROOT/code/reduction:+$PROJECT_ROOT/code/photometry
 export IDL_PATH
 
 export PYTHONPATH=$PYTHONPATH:$PROJECT_ROOT/code/photometry:$PROJECT_ROOT/code/photometry/dependencies:$PROJECT_ROOT/code/:$PROJECT_ROOT/code/reduction/
